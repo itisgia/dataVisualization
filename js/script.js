abf89bf2910031be0fdbf72d428059d7082051e3
jiah kwon <itisgia@gmail.com>
2018-08-26 17:27:42 +1200

    getting data is working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 console.log('linked');
-google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', {packages: ['corechart']});
+// google.charts.setOnLoadCallback(drawcharts);
 
-function drawChart() {
-	
+function drawCharts() {
+	$.ajax({
+	    url: "data/survey.json",
+	    dataType: "json",
+	    type: "GET",
+	    success: function functionName(data) {
+	    	console.log(data);
+			var data = new google.visualization.DataTable();
+			data.addColumn('number', 'People');
+			data.addColumn('string', 'Name');
+			data.addColumn('string', 'Gender');
+			data.addColumn('number', 'Age');
+			data.addColumn('string', 'Hours');
+			data.addColumn('number', 'Height');
+			data.addColumn('number', 'Multilingual');
+	    },
+	    error : function functionName(error) {
+	      console.log("ERROR");
+	      console.log(error);
+	    }
+	});
 }
+
+drawCharts();
